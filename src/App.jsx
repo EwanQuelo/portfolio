d53169be150a698c5c99bd5b7fdf6e80d6f158ce
Ewan QUELO <ewanquelo@aol.com>
2024-11-24 17:34:16 +0100

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,24 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <Titre/>
-        <Menu/>
-        
+        <div className='queryPhone'>
+          <Reseaux />
+          <Titre />
+          <Menu />
+        </div>
+       
+        <nav className="menuPhone">
+            <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" viewBox="0 0 24 24"><path fill="currentColor" d="M4 18h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1m0-5h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1s.45 1 1 1M3 7c0 .55.45 1 1 1h16c.55 0 1-.45 1-1s-.45-1-1-1H4c-.55 0-1 .45-1 1" /></svg>
+      
+                {/* <Reseaux/> */}
+                <Titre />
+                {/* <Menu /> */}
+        </nav>
+
       </header>
 
       <main>
-      <MenuProjet/>
-      <Reseaux/>
+        <MenuProjet />
       </main>
     </div>
   );
