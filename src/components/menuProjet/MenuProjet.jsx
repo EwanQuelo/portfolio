afb5d1f7749bd967487ed59613037729d3200cc7
Ewan QUELO <ewanquelo@aol.com>
2024-11-24 16:54:28 +0100

    v1.2

--- src/components/menuProjet/MenuProjet.jsx
+++ src/components/menuProjet/MenuProjet.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 
 const projects = [
     { title: "FLUX AI", date: "2024", description: "Chat utilisant l'api gemini" },
-    { title: "3D Tools", href: "https://ewanquelo.github.io/toolbox3d/", date: "2024", description: "Cube 3d d'outils" },
+    { title: "3D Tools", href: "", date: "2024", description: "Cube 3d d'outils" },
     { title: "Good or Not", date: "2024", description: "Jeu pour vision os" },
     { title: "V.OS Form", date: "2023", description: "Formulaires simple pour vision os" },
     { title: "Portfolio1", date: "2023", description: "Portfolio en html/css uniquement" },
-    { title: "weather app", href: "https://ewanquelo.github.io/easy-weather/", date: "2022", description: "app web html/css" },
-    { title: "calculatrice", href: "https://ewanquelo.github.io/calculatrice/", date: "2022", description: "calculatrice html/css" },
+    { title: "weather app", href: "", date: "2022", description: "app web html/css" },
+    { title: "calculatrice", href: "", date: "2022", description: "calculatrice html/css" },
 ];
 
 const Menu = () => {
